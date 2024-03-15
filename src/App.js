962d7af337d0b78dbfa07be140d5b09446224677
starboy <vkiplangat553@gmail.com>
2024-03-15 15:07:23 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
 
 export default function App() {
-  return <div>Hello World!</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
